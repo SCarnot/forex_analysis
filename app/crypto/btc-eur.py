b6411f43e20d93113fc899a5917429ae8715f97c
Matthieu <matthieusatan@hotmail.fr>
2020-09-13 23:57:20 +0200

    Adding script for cryptocurrencies

---
+++ app/crypto/btc-eur.py
@@ -0,0 +1,30 @@
+import utils
+import os
+import time
+import numpy as np
+
+url = 'https://api.kraken.com/0/public/Ticker'
+pair = 'XXBTZEUR'
+finename = '../data/XXBTZEUR.csv'
+
+if __name__ == '__main__':
+
+    #Create dump file if not created (not tracked by git)
+    if not os.path.exists('../data/'):
+        os.makedirs('../data/')
+
+    page, now = utils.get_api_result(url, pair)
+    header, data = utils.get_currency(page, now, pair)
+
+    with open(finename, 'wb') as f:
+        np.savetxt(f, data, header=header)
+        try:
+            while True:
+                page, now = utils.get_api_result(url, pair)
+                header, data = utils.get_currency(page, now, pair)
+                utils.dump_data(f, data)
+                print('{0} data loaded at '.format(pair), now.hour, ':', now.minute, ':', now.second)
+                time.sleep(30)
+
+        except KeyboardInterrupt:
+            print('Manual break by user')
---
+++ app/crypto/utils.py
@@ -0,0 +1,44 @@
+import json
+import requests
+import time
+import numpy as np
+import datetime
+
+def get_api_result(url, pair, timer_mean=0, timer_std=0.1):
+
+    # Waiting during a random time around timer_mean sec.
+    time.sleep(abs(np.random.randn()*timer_std + timer_mean))
+    # Getting the current time
+    now = datetime.datetime.now()
+    # Making the soup
+    data_url = url + '?pair={0}'.format(pair)
+    page = json.loads(requests.get(data_url).text)
+
+    return page, now
+
+def get_currency(page, now, pair):
+
+    dict_values = {
+        'year' : int(now.year),
+        'month' : int(now.month),
+        'day' : int(now.day),
+        'hour' : int(now.hour),
+        'minute' : int(now.minute),
+        'second' : int(now.second),
+        'ask' : page['result']['XXBTZEUR']['a'][0],
+        'bid' : page['result']['XXBTZEUR']['b'][0],
+        'last trade closed' : page['result']['XXBTZEUR']['c'][0],
+        'volume' : page['result']['XXBTZEUR']['v'][0],
+        'volume weighted average price' : page['result']['XXBTZEUR']['p'][0]
+    }
+    dict_values = {keys: np.float(value) for keys, value in dict_values.items()}
+    header = str(list(dict_values.keys()))[1:-1].replace('\'','')
+    data = np.array([list(dict_values.values())])
+
+    return header, data
+
+def dump_data(f, data):
+
+    np.savetxt(f, data)
+    f.flush()
+    time.sleep(0.1)
